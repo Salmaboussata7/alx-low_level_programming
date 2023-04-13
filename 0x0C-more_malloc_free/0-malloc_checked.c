654fb4c6e23e207eb7429cfec80021fe3530889a
salmaboussata7 <samos.boussata7@gmail.com>
2023-04-13 16:43:34 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,40 +1,18 @@
+#include "main.h"
 #include <stdlib.h>
-#include <string.h>
-
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+/**
+ * malloc_checked - allocates memory.
+ * @b: amount of bytes.
+ *
+ * Return: pointer to the allocated memory.
+ * if malloc fails, status value is equal to 98.
+ */
+void *malloc_checked(unsigned int b)
 {
-    unsigned int s1_len, s2_len, i;
-    char *result;
+  char *p;
 
-    if (s1 == NULL) {
-        s1 = "";
-    }
-
-    if (s2 == NULL) {
-        s2 = "";
-    }
-
-    s1_len = strlen(s1);
-    s2_len = strlen(s2);
-
-    if (n >= s2_len) {
-        n = s2_len;
-    }
-
-    result = malloc((s1_len + n + 1) * sizeof(char));
-    if (result == NULL) {
-        return NULL;
-    }
-
-    for (i = 0; i < s1_len; i++) {
-        result[i] = s1[i];
-    }
-
-    for (i = 0; i < n; i++) {
-        result[s1_len + i] = s2[i];
-    }
-
-    result[s1_len + n] = '\0';
-
-    return result;
+  p = malloc(b);
+  if (p == NULL)
+    exit(98);
+  return (p);
 }
