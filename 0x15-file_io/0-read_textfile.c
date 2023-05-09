eb866d2181df60d7f030bbe58477578f0c87dfda
salmaboussata7 <samos.boussata7@gmail.com>
2023-05-08 17:27:23 -0700

    0x15-file_io

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * read_file - reads a file and outputs its contents to stdout
+ * @filename: the name of the file to read
+ * @letters: the number of letters to read from the file
+ *
+ * Return: the number of letters read and printed
+ */
+ssize_t read_file(const char *filename, size_t letters)
+{
+int fd;
+char *buf;
+ssize_t rd, wr;
+
+if (!filename)
+return (0);
+
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
+
+buf = malloc(letters);
+if (!buf)
+{
+close(fd);
+return (0);
+}
+
+rd = read(fd, buf, letters);
+if (rd == -1)
+{
+free(buf);
+close(fd);
+return (0);
+}
+
+wr = write(STDOUT_FILENO, buf, rd);
+if (wr == -1)
+{
+free(buf);
+close(fd);
+return (0);
+}
+
+free(buf);
+close(fd);
+return (wr);
+}
